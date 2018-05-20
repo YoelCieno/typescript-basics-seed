daef233613cbba8e8e5b28e4b8fd6ec10c6dc330
Fernando <fernando@Fernandos-iMac.local>
2018-05-20 03:43:04 +0200

    frmonteira :: arrow functions

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('Hi, you, TypeScript!');
+const pizzas = [
+  { name: 'Pepperoni', toppings: ['pepperoni'] }
+];
+
+const mappedPizzas = pizzas.map(newPizza => newPizza.name.toUpperCase());
+
+console.log(mappedPizzas);
+
+const anotherPizza = {
+  name: 'Blazing Inferno',
+  getName: () => anotherPizza.name
+};
+
+console.log(anotherPizza.getName());
