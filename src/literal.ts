bfcfcac6570df8d46dc471340472cd876f061450
Fernando <fernando@Fernandos-iMac.local>
2018-05-31 03:05:09 +0200

    frmonteira :: comment error to a clean web console and add more literals js

--- src/literal.ts
+++ src/literal.ts
@@ -7,3 +7,13 @@ function selectSize(size: 'small' | 'medium' | 'large'): void {
 selectSize('large');
 
 console.log(`pizzaSize: ${pizzaSize}`);
+
+let pizzaSizeNum: number = 1;
+
+function selectSizeNum(sizeNum: 1 | 2 | 3): void {
+  pizzaSizeNum = sizeNum;
+}
+
+selectSizeNum(2);
+
+console.log(`pizzaSizeNum: ${pizzaSizeNum}`);
--- src/never.ts
+++ src/never.ts
@@ -1,6 +1,11 @@
+function orderError(error: string): void {
+  new Error(error);
+  // never going to return a value!  
+}
+/* If you want a break error:
 function orderError(error: string): never {
   throw new Error(error);
   // never going to return a value!  
 }
-
+*/
 orderError('Ohhh! Something went wrong');
